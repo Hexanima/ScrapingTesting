6e92bb7ab2778b621e810c7203a1f9216b908b0a
Hexanima <nicolasfilippelli22@gmail.com>
2026-01-21 12:44:00 -0300

    Changes implementation of index file

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import puppeteer from "puppeteer";
 import { ListProducts } from "./list-items.ts";
 import { LIST_CHANGE_TIMEOUT } from "./environment.ts";
 import { GenerateCSV } from "./generate-csv.ts";
+import { HandleClay } from "./handle-clay.ts";
+import { HandleCover } from "./handle-cover.ts";
 
 const browser = await puppeteer.launch({
   headless: false,
@@ -11,35 +13,56 @@ const browser = await puppeteer.launch({
 });
 const browserPage = await browser.newPage();
 
-const { items: covers } = await ListProducts(
-  {
-    browserPage,
-    timeout: LIST_CHANGE_TIMEOUT,
-  },
-  { page: "colores" }
-);
-
 const { items: clays } = await ListProducts(
   {
     browserPage,
     timeout: LIST_CHANGE_TIMEOUT,
   },
-  { page: "arcillas" }
+  { page: "arcillas" },
 );
-const coversData = covers.map((url) => ({ url }));
-await GenerateCSV({
-  headers: [{ key: "url", title: "URL" }],
-  items: coversData,
-  relativePath: "./covers.csv",
-});
-console.log("Archivo covers.csv creado exitosamente");
-
-const claysData = clays.map((url) => ({ url }));
+const claysData = await HandleClay({ browserPage }, { pageLinks: clays });
 console.log("Archivo clays.csv creado exitosamente");
 await GenerateCSV({
   relativePath: "./clays.csv",
-  headers: [{ key: "url", title: "URL" }],
-  items: claysData,
+  headers: [
+    { key: "url", title: "URL" },
+    { key: "name", title: "Titulo" },
+    { key: "price", title: "Precio (€)" },
+    { key: "color", title: "Color" },
+    { key: "temperature", title: "Temperatura (°C)" },
+    { key: "description", title: "Descripción" },
+  ],
+  items: claysData.map((clay) => ({
+    ...clay,
+    temperature: `${clay.temperature.min}-${clay.temperature.max}`,
+  })),
+}); 
+
+const { items: covers } = await ListProducts(
+  {
+    browserPage,
+    timeout: LIST_CHANGE_TIMEOUT,
+  },
+  { page: "colores" },
+);
+const coversData = await HandleCover({ browserPage }, { pageLinks: covers });
+await GenerateCSV({
+  headers: [
+    { key: "url", title: "URL" },
+    { key: "image", title: "Imagen (URL)" },
+    { key: "name", title: "Titulo" },
+    { key: "price", title: "Precio (€)" },
+    { key: "color", title: "Color" },
+    { key: "temperature", title: "Temperatura (°C)" },
+    { key: "type", title: "Tipo" },
+    { key: "description", title: "Descripción" },
+  ],
+  items: coversData.map((cover) => ({
+    ...cover,
+    temperature: `${cover.temperature.min}-${cover.temperature.max}`,
+  })),
+  relativePath: "./covers.csv",
 });
+console.log("Archivo covers.csv creado exitosamente");
 
 await browser.close();
